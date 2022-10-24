be58b71ff5392f52985cf8fa86650d66d6ac8155
JustinG512 <JustinGallagher512@gmail.com>
2022-10-24 19:30:54 -0400

    update files

--- bin/tictactoe.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import ConfigProvider from '@config/ConfigProvider';
-import TicTacToe from '../src';
-
-new TicTacToe(new ConfigProvider())
-    .login()
-    .then(() => console.log('TicTacToe module ready to be used. Check your Discord server!'));
