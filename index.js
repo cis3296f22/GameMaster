23b9b4b6144c56bf157ca9eec28a1d77a7b6c2b1
JustinG512 <JustinGallagher512@gmail.com>
2022-10-24 19:02:28 -0400

    remove not needed

--- index.js
+++
@@ -1,5 +0,0 @@
-const TicTacToe = require('discord-tictactoe');
-// set "command" to an empty string and "textCommand" with prefix you want
-new TicTacToe({ textCommand: '-tictactoe', command: '' })
-   .login(process.env.TOKEN)
-   .then(() => console.log('TicTacToe bot is ready to be used.'));
