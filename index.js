928cc7d35b9fd6716f7b140aa07bd283e7688f61
KAAzadi <70913312+KAAzadi@users.noreply.github.com>
2022-11-01 23:44:02 -0400

    Refactoring and Shifting of Files
    Did some mild refactoring that primarily consisted of renaming variables to better fit their roles. Additionally, repositioned some files to better organize the repo.

--- index.js
+++ index.js
@@ -1,8 +1,52 @@
 const TicTacToe = require('discord-tictactoe');
+const Discord = require('discord.js');
+//
+const dotenv = require('dotenv');
+dotenv.config();
 const TOKEN = process.env.TOKEN;
+const GUILDID = process.env.GUILDID;
 
-// set "command" to an empty string and "textCommand" with prefix you want
-new TicTacToe({ textCommand: '-tictactoe', command: '' })
-    
-   .login("TOKEN HERE")
-   .then(() => console.log('TicTacToe bot is ready to be used.'));
+// ... THIS WILL SHOW TOKEN.  Do not use in public
+// console.log(process.env.TOKEN);
+// console.log(process.env.GUILDID);
+
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ],
+});
+const game = new TicTacToe({ language: 'en', commandOptionName: 'user' });
+
+client.on('ready', () => {
+    // Register your command
+    console.log("Bot online")
+    client.application.commands.create(
+        {
+            name: 'tictactoe',
+            description: 'Play tictactoe',
+            options: [
+                {
+                    type: 'USER',
+                    name: 'user',
+                    description: "Mention the User",
+                    required: false
+                }
+            ]
+
+        },
+        GUILDID,
+        console.log("Command: -tictacttoe registered")
+    );
+
+
+    // Listening for interactions
+    client.on('interactionCreate', interaction => {
+        if (interaction instanceof Discord.CommandInteraction && interaction.commandName === 'tictactoe') {
+            console.log("Received: -tictacttoe from user")
+            game.handleInteraction(interaction);
+        }
+    });
+});
+
+client.login(MTAzNDUwMzU5MTY2ODQ4NjE3Ng.GO95Ky.IrxFPl-RxwNvM4dfqTjSoGQgWXBz-6Jbl4kZCU);
