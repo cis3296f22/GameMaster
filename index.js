fda337d5f71f3260d7c70c9750406559744e5b74
jay newman <tup16252@temple.edu>
2022-11-04 17:07:26 -0400

    Added slash command for connect 4. Currently just runs tictactoe. Will try to clone and rename connect 4 then implement trickle down.

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ dotenv.config();
 const TOKEN = process.env.TOKEN;
 const GUILDID = process.env.GUILDID;
 
-// ... THIS WILL SHOW TOKEN.  Do not use in public
-// console.log(process.env.TOKEN);
-// console.log(process.env.GUILDID);
-
 const client = new Discord.Client({
   intents: [
     Discord.Intents.FLAGS.GUILDS,
     Discord.Intents.FLAGS.GUILD_MESSAGES
   ],
 });
-const game = new TicTacToe({ language: 'en', commandOptionName: 'user' });
+// const game = new TicTacToe({ language: 'en', commandOptionName: 'user' });
 
 client.on('ready', () => {
     // Register your command
@@ -37,12 +33,32 @@ client.on('ready', () => {
         GUILDID,
     console.log("Command: -tictacttoe registered")
     );
-    
+    client.application.commands.create(
+        {
+            name: 'connectfour',
+            description: 'Play Connect Four',
+            options: [
+                {
+                    type: 'USER',
+                    name: 'user',
+                    description: "Mention the User",
+                    required: false
+                }
+            ]            
+        },
+        GUILDID,
+    console.log("Command: -connectfour registered")
+    );
 
-    // Listening for interactions
-    client.on('interactionCreate', interaction => {
+client.on('interactionCreate', interaction => {
         if (interaction instanceof Discord.CommandInteraction && interaction.commandName === 'tictactoe') {
             console.log("Received: -tictacttoe from user")
+            const game = new TicTacToe({ language: 'en', commandOptionName: 'user' });
+            game.handleInteraction(interaction);
+        }
+        if (interaction instanceof Discord.CommandInteraction && interaction.commandName === 'connectfour') {
+            console.log("Received: -connectfour from user")
+            const game = new TicTacToe({ language: 'en', commandOptionName: 'user' });
             game.handleInteraction(interaction);
         }
     });
