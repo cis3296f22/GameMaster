6a9383a79f3ca379e716c0145297281810b695b1
Jay Newman <tup16252@temple.edu>
2022-11-14 15:14:36 -0500

    Added deck of cards images into folder cardDeck. Can use images for card games. hilo currently returns random card image

--- slashcommands/hilo.js
+++ slashcommands/hilo.js
@@ -1,5 +1,20 @@
 const run = async (client, interaction) => {
-    return interaction.reply("Pong!");
+
+    const cardDeck = ["cardDeck/AD.png", "cardDeck/AC.png", "cardDeck/AH.png", "cardDeck/AS.png", "cardDeck/2D.png", "cardDeck/2C.png", "cardDeck/2H.png",
+              "cardDeck/2S.png", "cardDeck/3D.png", "cardDeck/3C.png", "cardDeck/3H.png", "cardDeck/3S.png",
+              "cardDeck/4D.png", "cardDeck/4C.png", "cardDeck/4H.png", "cardDeck/4S.png", "cardDeck/5D.png", "cardDeck/5C.png", "cardDeck/5H.png",
+              "cardDeck/5S.png", "cardDeck/6D.png", "cardDeck/6C.png", "cardDeck/6H.png", "cardDeck/6S.png",
+              "cardDeck/7D.png", "cardDeck/7C.png", "cardDeck/7H.png", "cardDeck/7S.png", "cardDeck/8D.png", "cardDeck/8C.png", "cardDeck/8H.png",
+              "cardDeck/8S.png", "cardDeck/9D.png", "cardDeck/9C.png", "cardDeck/9H.png", "cardDeck/9S.png",
+              "cardDeck/10D.png", "cardDeck/10C.png", "cardDeck/10H.png",
+              "cardDeck/10S.png", "cardDeck/JD.png", "cardDeck/JC.png", "cardDeck/JH.png", "cardDeck/JS.png", "cardDeck/QD.png", "cardDeck/QC.png",
+              "cardDeck/QH.png", "cardDeck/QS.png",
+              "cardDeck/KD.png", "cardDeck/KC.png", "cardDeck/KH.png", "cardDeck/KS.png"];
+
+
+    const randomCard = cardDeck[Math.floor(Math.random() * cardDeck.length)];
+
+    return interaction.reply({ files: [{ attachment: randomCard }] }); ;
 }
 
 
